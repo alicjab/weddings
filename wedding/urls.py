fd6541df96ad07303b6bcc1951ee18e7585c7b54
Alicja Baszak <alicjabaszak@gmail.com>
2016-10-31 02:31:48 +0100

    First css and gift detail

--- wedding/urls.py
+++ wedding/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.gift_list, name='gift_list'),
+    url(r'^gift/(?P<id>[0-9]+)/$', views.gift_detail, name='gift_detail'),
+
 ]
--- wedding/views.py
+++ wedding/views.py
@@ -1,6 +1,8 @@
 #-*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 from .models import Gift
 
@@ -8,3 +10,7 @@ def gift_list(request):
     new_gifts = Gift.objects.filter(accepted=False)
     accepted_gifts = Gift.objects.filter(accepted=True)
     return render(request, 'wedding/gift_list.html', {'new_gifts' : new_gifts, 'accepted_gifts' : accepted_gifts})
+
+def gift_detail(request, id):
+    gift = get_object_or_404(Gift, id=id)
+    return render(request, 'wedding/gift_detail.html', {'gift' : gift})
